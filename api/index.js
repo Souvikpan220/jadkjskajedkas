85f3490bba2217984f683e5d35f3510ac852819a
Souvikpan220 <117498210+Souvikpan220@users.noreply.github.com>
2026-04-09 13:57:19 +0530

    Update index.js

--- api/index.js
+++ api/index.js
@@ -1,1 +1,13 @@
+import express from "express";
 
+const app = express();
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.json({ message: "API working" });
+});
+
+// ❌ REMOVE app.listen()
+
+export default app;
